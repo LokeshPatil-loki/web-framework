02ee31de89d2dd5892947ca6e7bc2cea498433ff
LokeshPatil-loki <lokesh1404@outlook.com>
2022-12-24 12:20:27 +0530

    Sync and Attributes Deligation in Users

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,37 +1,20 @@
 import axios, { AxiosResponse } from "axios";
+import { Attributes } from "./Attributes";
 import { Eventnig } from "./Eventing";
+import { Sync } from "./Sync";
 
-interface UserProps {
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
 }
+const rootUrl: string = "http://localhost:3000/users";
 
 export class User {
-  private url: string = "http://localhost:3000/users";
-  events: Eventnig = new Eventnig();
-  constructor(private data: UserProps) {}
-
-  get(propName: string): string | number {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
-
-  fetch(): void {
-    axios.get(`${this.url}/${this.get("id")}`).then((response: AxiosResponse): void => {
-      this.set(response.data);
-    });
-  }
-
-  save(): void {
-    const id = this.get("id");
-    if (id) {
-      axios.put(`${this.url}/${id}`, this.data);
-    } else {
-      axios.post(this.url, this.data);
-    }
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public events: Eventnig = new Eventnig();
+  public attributes: Attributes<UserProps>;
+  constructor(attr: UserProps) {
+    this.attributes = new Attributes<UserProps>(attr);
   }
 }
